97325f1bf144dc9a89d895a894dea872f524aac7
sniki-ld <dennisalenaf@gmail.com>
2020-12-23 13:53:23 +0300

    task_5

---
+++ task_5.py
@@ -0,0 +1,10 @@
+# Даны катеты прямоугольного треугольника.
+# Найти его гипотенузу и площадь.
+
+leg_1 = 8
+leg_2 = 5
+tr_s = (leg_1 * leg_2) / 2
+hyp = (leg_1 ** 2 + leg_2 ** 2) ** 0.5
+print('Area of ​​a triangle :', tr_s)
+print('Hypotenuse of triangle :', hyp)
+
