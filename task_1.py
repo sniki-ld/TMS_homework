67b54c49a572548489a0132557ab96824cdca921
sniki-ld <dennisalenaf@gmail.com>
2020-12-23 13:20:33 +0300

    task_1

---
+++ task_1.py
@@ -0,0 +1,9 @@
+# Даны 2 действительных числа a и b. Получить их сумму, разность и произведение.
+a = 63
+b = 37
+sum = a + b
+mult = a * b
+sub = a - b
+print('Sum :', sum)
+print('Multiplication :', mult)
+print('Subtraction :', sub)
