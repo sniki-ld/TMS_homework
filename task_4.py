fee72238585c178ba686b0a87a5d1344e348cc69
sniki-ld <dennisalenaf@gmail.com>
2020-12-23 13:51:23 +0300

    task_4

---
+++ task_4.py
@@ -0,0 +1,9 @@
+# Даны два действительных числа.
+# Найти среднее арифметическое и среднее геометрическое этих чисел.
+import math
+numb_1 = 87
+numb_2 = 63
+a_mean = (numb_1 + numb_2) / 2
+g_mean = (numb_1 * numb_2) ** 0.5
+print('Arithmetical mean :', a_mean)
+print('Geometric mean :', g_mean)
