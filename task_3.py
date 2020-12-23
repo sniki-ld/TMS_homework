8e621c7ecb4b0ba6bffad3875fd3ec016e206e19
sniki-ld <dennisalenaf@gmail.com>
2020-12-23 13:49:16 +0300

    task_3

---
+++ task_3.py
@@ -0,0 +1,7 @@
+# Дана длина ребра куба. Найти объем куба и площадь его боковой поверхности.
+ed_len = 6
+cube_v = ed_len ** 3
+cube_s = ed_len ** 2
+print('Cube volume :', cube_v)
+print('Cube area :', cube_s)
+
