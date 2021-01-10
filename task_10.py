ce94dec023932f8614d1abff4c825b18d5d4c9b1
sniki-ld <dennisalenaf@gmail.com>
2021-01-10 09:02:23 +0300

    1)add while solution 2)add for solution

--- task_10.py
+++ task_10.py
@@ -8,3 +8,24 @@ new_list = [(i * -2) for i in my_list]
 
 print(new_list)
 
+# решение через  while
+
+len_list = len(my_list)
+i = 0
+new_list = []
+while i < len_list:
+    new_list.append(my_list[i] * (-2))
+    i += 1
+
+print(new_list)
+
+# решение через  for
+
+my_list = [2, 8, 7, 9, 12]
+
+len_list = len(my_list)
+new_list = []
+for i in my_list:
+    new_list.append(i * -2)
+
+print(new_list)
