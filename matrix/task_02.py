83abe78271c42a17ddf68293870b1f39e0c6ae31
sniki-ld <dennisalenaf@gmail.com>
2021-01-11 09:40:01 +0300

    add matrix_a and matrix_b

--- matrix/task_02.py
+++ matrix/task_02.py
@@ -3,4 +3,33 @@
 # a.создать матрицу равную сумме matrix_a и matrix_b;
 # b.создать матрицу равную разности matrix_a и matrix_b;
 # c.создать новую матрицу равную matrix_a умноженной на число g.
-# g вводится с клавиатуры.
+# g вводится с клавиатуры
+
+
+from random import randint
+
+my_matrix_a = []
+my_matrix_b = []
+n = int(input())
+m = int(input())
+
+print('Матрица matrix_a:')
+
+for i in range(n):
+    row = []
+    for j in range(m):
+        row.append(randint(1, 10))
+    my_matrix_a.append(row)
+for i in my_matrix_a:
+    print(i)
+
+print('Матрица matrix_b:')
+
+for i in range(n):
+    row = []
+    for j in range(m):
+        row.append(randint(0, 18))
+    my_matrix_b.append(row)
+for i in my_matrix_b:
+    print(i)
+
